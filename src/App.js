088a99609615eb21875a4d46b211b03302b4d6ac
WilliamSilva <williamsilva20062005@gmail.com>
2024-02-05 08:12:44 -0300

    Criado a Navbar e começo de estilização e integração do conteúdo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="main"></div>;
+  return <div class="main"></div>;
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar-main">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;
